a00f1209218fa417e192d1b74eae7fd7eab65cd0
Maleehak <39943018+Maleehak@users.noreply.github.com>
2020-01-10 10:25:22 +0500

    Removed arrays and used vectors Removed sorting algorithm and reversed the loop

--- HashCodeProblem/Source.cpp
+++ HashCodeProblem/Source.cpp
@@ -1,61 +1,60 @@
 #include <iostream>
+#include <vector>
+#include <fstream>
 using namespace std;
 void slicesOrdered(int, int, int*);  //actual function that prints out slices sum and which pizza to get
-void swap(int&a, int& b);// helper function in sorting algorithm
-void sortInDecresingOrder(int* slicesOfPizza,int typesOfPizzas); //sorts the pizza slices from max to min
+void slicesOrdered( ifstream&);
+//void swap(int&a, int& b);// helper function in sorting algorithm
+void sortInDecreasingOrder(vector<pair<int,int>> &slicesOfPizza, int&typesOfPizzas);
 int main() {
+	ifstream f;
+	f.open("Input.txt");
+	if (f.is_open()) {
+		slicesOrdered(f);
+		f.close();
+	}
+	else {
+		cout << "Errror";
+	}
 	int maxSlices = 17;
 	int typesOfPizzas = 4;
 	int slicesInPizzas[4] = { 2,5,6,8 };
-	slicesOrdered(maxSlices, typesOfPizzas, slicesInPizzas);
+	//slicesOrdered(maxSlices, typesOfPizzas, slicesInPizzas);
 
 }
 
-void swap(int& a, int& b)
-{
-	int temp = 0;
-	temp = b;
-	b = a;
-	a = temp;
-}
-
-void sortInDecresingOrder(int* slicesOfPizza,int typesOfPizzas) {
-	//Bubble sort
-	int i, j;
-	for (i = 0; i < typesOfPizzas - 1; i++)
-		for (j = 0; j < typesOfPizzas - i - 1; j++)
-			if (slicesOfPizza[j] < slicesOfPizza[j + 1])
-				swap(slicesOfPizza[j], slicesOfPizza[j + 1]);
-}
-
-void slicesOrdered(int maxSlices, int typeOfPizzas, int* slicesInPizzas) {
-	int* orderedSlices = new int[typeOfPizzas]; //contains the slices of pizzas to include
-	int* tempArray = new int[typeOfPizzas]; //temporary array to apply sorting algorithm to not affect the actual array
-	//initializing both arrays
-	for (int i = 0; i < typeOfPizzas; i++) {
-		orderedSlices[i] = -1;
-		tempArray[i] = slicesInPizzas[i];
+void slicesOrdered( ifstream& fin) {
+	vector<pair<int, int>>slicesInPizzas;
+	int maxSlices = 0;
+	int getValue=0;
+	int typeOfPizzas = 0;
+	fin >> maxSlices;
+	fin >> typeOfPizzas;
+	int i = 0;
+	while(!(fin.eof())) {
+		fin>> getValue;
+		slicesInPizzas.push_back(make_pair(getValue,i));
+		i++;
 	}
-	//sorting tempArray
-	sortInDecresingOrder(tempArray, typeOfPizzas);
-	//takes the maximum
+	vector<pair<int, int>>orderedSlices;
 	int slicesSum = 0;
 	int slicesSumTemp = 0;
-	for (int i = 0; i < typeOfPizzas; i++) {
-		slicesSumTemp =slicesSum+tempArray[i];
+	int temp1, temp2;
+	for (int i = typeOfPizzas-1; i >=0; i--) {
+		slicesSumTemp = slicesSum + slicesInPizzas[i].first;
 		if (slicesSumTemp <= maxSlices) {
-			orderedSlices[i] = tempArray[i];
+			temp1 = slicesInPizzas[i].first;
+			temp2 = slicesInPizzas[i].second;
 			slicesSum = slicesSumTemp;
+			orderedSlices.push_back(make_pair(temp1,temp2));
 		}
-
-	}
-	//match the values in orderedSlices and slicesInPizzas the print out the index of slicesInPizzas
-	for (int i =  typeOfPizzas; i >=0; i--) {
-		for (int j = 0; j < typeOfPizzas; j++) {
-			if (orderedSlices[i]==slicesInPizzas[j] ) {
-				cout << j;
-			}
-		}
-	}
 		
+	}
+	for (int i = orderedSlices.size()-1; i >= 0; i--) {
+		cout << "S"<<orderedSlices[i].second<<endl;
+	}
+
+	
+	
 }
+
